f8fd152a27a5040c92956f9a58ed671e1e4e13c0
Sergey Petrelevich <petrelevich@yandex.ru>
2020-01-10 19:28:28 +0300

    L02 shade

---
+++ L02-shade/src/main/java/ru/otus/Main.java
@@ -0,0 +1,23 @@
+package ru.otus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sergey
+ * created on 01.07.18.
+ */
+
+/*
+Useful link:
+https://maven.apache.org/plugins/maven-shade-plugin/index.html
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> example = new ArrayList<>();
+        System.out.println(example);
+
+        System.out.println("Hello, World. Shade test!");
+    }
+}
